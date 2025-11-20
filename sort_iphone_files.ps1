0dfbff8d5e5326df0f1ff8dba4147a27688596e8
dang <79184762+dang0x5f@users.noreply.github.com>
2025-11-19 21:23:11 -0500

    dynamic directory names
    remarks: removed hardcode filename values (i.e., AAE , JPG , HEIC , etc.)

--- sort_iphone_files.ps1
+++ sort_iphone_files.ps1
@@ -3,12 +3,15 @@
 # - add timer     #
 # - add counter   #
 # - add usage()   #
+# - user input:   #
+#   cli , gui     #
 # *************** #
 
 param(
    [string]$dir
 )
 
+$ext_list = @{}
 $destination = "$home\pictures\iphone"
 
 function traverse_dir {
@@ -23,18 +26,15 @@ function traverse_dir {
 	 traverse_dir($path)
       }else{
          $ext = (get-item -path $path).extension.substring(1).toupper()
-	 move-item -path $path -destination $destination-$ext
-	 #write $path
+	 if($ext_list["$ext"] -eq $null){
+            mkdir $destination-$ext 2>$null
+	    $ext_list["$ext"] = 1
+	 }
+         move-item -path $path -destination $destination-$ext
       }
    }
 }
 
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-
-$ext_list = @("HEIC", "PNG", "JPG", "MOV", "AAE")
-
-foreach($ext in $ext_list){
-   mkdir $destination-$ext 2>$null
-}
 
 traverse_dir($dir)
